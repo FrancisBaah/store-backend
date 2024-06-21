e1cb6b561c97d1245865c0358d8a412d043cf82a
FrancisBaah <baahfrancis88@gmail.com>
2024-06-21 14:26:01 +0400

    --

--- Collections/UserCollection.js
+++ Collections/UserCollection.js
@@ -70,6 +70,12 @@ const getUser = asyncHandler(async (req, res) => {
   res.status(200).json(req.user);
 });
 
+//all users
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
+
 // Generate JWT
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -81,4 +87,5 @@ module.exports = {
   registerUser,
   loginUser,
   getUser,
+  getAllUsers,
 };
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -3,11 +3,13 @@ const {
   registerUser,
   loginUser,
   getUser,
+  getAllUsers,
 } = require("../Collections/UserCollection");
 const { protect } = require("../Middleware/AuthHandler");
 const router = express.Router();
 
 router.post("/", registerUser);
+router.get("/all", getAllUsers);
 router.post("/login", loginUser);
 router.get("/", protect, getUser);
 
