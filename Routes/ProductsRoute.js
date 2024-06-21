c47c6a29e4f69ae9a6213d04a91fd933079418cb
FrancisBaah <baahfrancis88@gmail.com>
2024-06-21 12:21:48 +0400

    --

--- Routes/ProductsRoute.js
+++ Routes/ProductsRoute.js
@@ -4,12 +4,13 @@ const {
   addProduct,
 } = require("../Collections/ProductsCollection");
 const multer = require("multer");
+const path = require("path");
 const router = express.Router();
 
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "../uploads"));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
@@ -21,4 +22,5 @@ const upload = multer({ storage: storage });
 // Routes
 router.get("/", getAllProducts);
 router.post("/", upload.single("image"), addProduct);
+
 module.exports = router;
