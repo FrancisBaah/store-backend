e84496a640d1170a69531ac080274db16bf28274
FrancisBaah <baahfrancis88@gmail.com>
2024-07-17 10:18:04 +0400

    cloudinary

--- Routes/ProductsRoute.js
+++ Routes/ProductsRoute.js
@@ -5,14 +5,23 @@ const {
 } = require("../Collections/ProductsCollection");
 const multer = require("multer");
 const router = express.Router();
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Configure multer for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "storeuploads/",
+    format: async (req, file) => "png", // or dynamically determine format
+    public_id: (req, file) => file.originalname.split(".")[0], // Using file name without extension
   },
 });
 
@@ -21,4 +30,11 @@ const upload = multer({ storage: storage });
 // Routes
 router.get("/", getAllProducts);
 router.post("/", upload.single("image"), addProduct);
+
+// Error handling middleware
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({ error: "An error occurred, please try again later." });
+});
+
 module.exports = router;
